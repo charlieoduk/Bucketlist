248dc2572505c707aebfa32141ad56695e953a54
charlieoduk <odukjr@gmail.com>
2017-07-17 16:18:58 +0300

    Initialize sqlalchemy and create a new Flask application

--- bucketlist/__init__.py
+++ bucketlist/__init__.py
@@ -1,18 +1,16 @@
 from flask import Flask
-from flask_restful import reqparse, abort, Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 
 # local import
-from instance.config import app_config
+from .config import app_config
 
 # initialize sql-alchemy
 db = SQLAlchemy()
 
+
 def create_app(config_name):
     app = Flask(__name__)
-    api = Api(app)
     app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
