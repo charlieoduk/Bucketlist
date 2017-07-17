9d4a6af56e27372173fe7c58f73322d900fc6ba2
charlieoduk <odukjr@gmail.com>
2017-07-17 16:20:23 +0300

    Set the various environments

--- bucketlist/config.py
+++ bucketlist/config.py
@@ -1,30 +1,37 @@
 import os
 
+
 class Config(object):
     """Parent configuration class."""
     DEBUG = False
     CSRF_ENABLED = True
     SECRET = os.getenv('SECRET')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/bucketlist'
+
 
 class DevelopmentConfig(Config):
     """Configurations for Development."""
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/bucketlist'
+
 
 class TestingConfig(Config):
     """Configurations for Testing, with a separate test database."""
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/bucketlist_test'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/bucketlist'
     DEBUG = True
+
 
 class StagingConfig(Config):
     """Configurations for Staging."""
     DEBUG = True
 
+
 class ProductionConfig(Config):
     """Configurations for Production."""
     DEBUG = False
     TESTING = False
+
 
 app_config = {
     'development': DevelopmentConfig,
