e906a21f14435cab8ce37239424baa260eeed943
charlieoduk <odukjr@gmail.com>
2017-07-18 07:21:41 +0300

    Add login and authentication fields in models

--- bucketlist/models.py
+++ bucketlist/models.py
@@ -1,22 +1,54 @@
+import pytz
+
 from datetime import datetime
+from passlib.apps import custom_app_context as pwd_context
+from itsdangerous import (TimedJSONWebSignatureSerializer
+                          as Serializer, BadSignature, SignatureExpired)
+
 
 from bucketlist import db
+from .config import Config
+
+kenyan_time = pytz.timezone('Africa/Nairobi')
 
 
 class User(db.Model):
     """This class represents the user table"""
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
+    name = db.Column(db.String(32))
+    password_hash = db.Column(db.String(68))
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
     date_modified = db.Column(
-        db.DateTime, default=db.func.current_timestamp(),
-        onupdate=db.func.current_timestamp())
+        db.DateTime, default=datetime.now(kenyan_time),
+        onupdate=datetime.now(kenyan_time))
     bucketlists = db.relationship('Bucketlist', backref='user',
                                   cascade="all,delete")
 
     def __init__(self, name):
         self.name = name
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    def generate_auth_token(self, expiration=600):
+        s = Serializer(Config['SECRET_KEY'], expires_in=expiration)
+        return s.dumps({'id': self.id})
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(Config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return None    # valid token, but expired
+        except BadSignature:
+            return None    # invalid token
+        user = User.query.get(data['id'])
+        return user
 
     def save(self):
         db.session.add(self)
