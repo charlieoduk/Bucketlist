653c9459269f71cb2e3328bfa2e3d4a3664318fc
charlieoduk <odukjr@gmail.com>
2017-07-19 11:44:04 +0300

    Add create app method to the config file

--- bucketlist/config.py
+++ bucketlist/config.py
@@ -1,4 +1,7 @@
 import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api
 
 
 class Config(object):
@@ -39,3 +42,19 @@ app_config = {
     'staging': StagingConfig,
     'production': ProductionConfig,
 }
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(app_config[config_name])
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    return app
+
+
+app = create_app(config_name=os.getenv('APP_SETTINGS'))
+
+# create flask restful api
+api = Api(app)
+
+# initialize sql-alchemy
+db = SQLAlchemy(app)
