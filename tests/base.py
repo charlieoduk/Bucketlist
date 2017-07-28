57c950787529ec5fc935e4a3e7b46b53148ea38e
charlieoduk <odukjr@gmail.com>
2017-07-28 12:10:12 +0300

    Add test setup

--- tests/base.py
+++ tests/base.py
@@ -1,3 +1,4 @@
+import json
 from flask_testing import TestCase
 
 from bucketlist import app, db
@@ -9,6 +10,30 @@ class BaseTestCase(TestCase):
     def create_app(self):
         app.config.from_object('bucketlist.config.TestingConfig')
         return app
+
+    def register_user(self, name, email, password):
+        return self.client.post(
+            '/api/v1/auth/register/',
+            data=json.dumps(dict(
+                name=name,
+                email=email,
+                password=password
+            )),
+            content_type='application/json',
+        )
+
+    def base_registration(self):
+        return self.register_user('Joe', 'joe@gmail.com', '123456')
+
+    def login_user(self, email, password):
+        return self.client.post(
+            '/api/v1/auth/login/',
+            data=json.dumps(dict(
+                email=email,
+                password=password
+            )),
+            content_type='application/json',
+        )
 
     def setUp(self):
         db.create_all()
