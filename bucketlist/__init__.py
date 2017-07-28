ff98a4a4ac30d57f2f4d76c10cf464133be20706
charlieoduk <odukjr@gmail.com>
2017-07-28 12:11:35 +0300

    Add bucketlist and items views

--- bucketlist/__init__.py
+++ bucketlist/__init__.py
@@ -1,16 +1,16 @@
 import os
 
 from flask import Flask
-from flask_httpauth import HTTPBasicAuth
+from flask_httpauth import HTTPTokenAuth
 from flask_sqlalchemy import SQLAlchemy
-from flask_restful import Api
+from flask_restplus import Api
 
 from .config import app_config
 
 # initialize sql-alchemy
 db = SQLAlchemy()
 # initialize HTTP auth
-auth = HTTPBasicAuth()
+authentication = HTTPTokenAuth(scheme='Token')
 
 
 def create_app(config_name):
@@ -19,15 +19,30 @@ def create_app(config_name):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
+
     from bucketlist.auth import auth as auth_blueprint
     api = Api(auth_blueprint)
-    app.register_blueprint(auth_blueprint, url_prefix='/api/v1')
+    app.register_blueprint(auth_blueprint, url_prefix='/api/v1.0')
     from bucketlist.auth.views import CreateUser, LogUserIn
     api.add_resource(CreateUser, '/auth/register/')
     api.add_resource(LogUserIn, '/auth/login/')
+
+    from bucketlist.bucketlists import bucketlists as bucketlists_blueprint
+    api = Api(bucketlists_blueprint)
+    app.register_blueprint(bucketlists_blueprint, url_prefix='/api/v1.0')
+    from bucketlist.bucketlists.views import BucketListItemsResource, BucketListResource
+    api.add_resource(BucketListResource, '/bucketlists/')
+    api.add_resource(BucketListItemsResource,
+                     '/bucketlists/<int:bucketlist_id>/')
+
+    from bucketlist.items import items as items_blueprint
+    api = Api(items_blueprint)
+    app.register_blueprint(items_blueprint, url_prefix='/api/v1.0')
+    from bucketlist.items.views import ItemsResource, ItemsList
+    api.add_resource(ItemsResource, '/bucketlists/<id>/items/')
+    api.add_resource(ItemsList, '/bucketlists/<id>/items/<item_id>')
 
     return app
 
 
 app = create_app(config_name=os.getenv('APP_SETTINGS'))
-test_app = create_app(config_name='testing')
