57a91d4596060a4c6c3588f47bc6ba00ecde44f3
charlieoduk <odukjr@gmail.com>
2017-07-31 11:21:37 +0300

    [chore] Add new base test cases

--- tests/base.py
+++ tests/base.py
@@ -13,7 +13,7 @@ class BaseTestCase(TestCase):
 
     def register_user(self, name, email, password):
         return self.client.post(
-            '/api/v1/auth/register/',
+            '/api/v1.0/auth/register/',
             data=json.dumps(dict(
                 name=name,
                 email=email,
@@ -27,13 +27,54 @@ class BaseTestCase(TestCase):
 
     def login_user(self, email, password):
         return self.client.post(
-            '/api/v1/auth/login/',
+            '/api/v1.0/auth/login/',
             data=json.dumps(dict(
                 email=email,
                 password=password
             )),
             content_type='application/json',
         )
+
+    def base_authentication(self):
+        self.base_registration()
+        login = self.login_user(
+            email='joe@gmail.com', password='123456')
+        token = json.loads(login.data.decode())
+        user_token = token['token']
+        return user_token
+
+    def base_add_bucketlist(self):
+        user_token = self.base_authentication()
+        self.client.post(
+            '/api/v1.0/bucketlists/',
+            data=json.dumps(dict(
+                name="new bucketlist",
+            )),headers={'Authorization' : user_token},
+        )
+        return user_token
+
+    def base_add_bucketlist2(self):
+        user_token = self.base_authentication()
+        self.client.post(
+            '/api/v1.0/bucketlists/',
+            data=json.dumps(dict(
+                name="Second bucketlist",
+            )),headers={'Authorization' : user_token},
+        )
+        return user_token
+
+    def base_add_item(self):
+        user_token = self.base_add_bucketlist()
+
+        self.client.post(
+            '/api/v1.0/bucketlists/1/items/',
+            data=json.dumps(dict(
+                name="Bungee jump",
+            )),headers={'Authorization' : user_token},
+        )
+
+        return user_token
+
 
     def setUp(self):
         db.create_all()
