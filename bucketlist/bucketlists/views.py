19f9608a0dad60ebca78cf87482af9ef5f6f19ef
charlieoduk <odukjr@gmail.com>
2017-08-07 11:59:10 +0300

    FIX -Update docstrings for documentation

--- bucketlist/bucketlists/views.py
+++ bucketlist/bucketlists/views.py
@@ -1,7 +1,7 @@
-from flask import request
-from flask_restplus import Resource, fields, marshal_with, abort, marshal, reqparse
+from flask import request, Flask
+from flask_restplus import Resource, fields, marshal_with, abort, marshal, reqparse, Api
+from flask_sqlalchemy import BaseQuery
 
-from bucketlist.bucketlists.parsers import parser
 # local import
 from bucketlist.models import Bucketlist, BucketListItems, User
 from bucketlist.items.views import item_fields
@@ -22,17 +22,18 @@ class BucketListResource(Resource):
 
     @marshal_with(bucketlist_fields)
     def get(self):
-        '''Returns a collection of bucketlists that belong to the user
+        '''
 
-        .. :quickref: BucketListResource; Get a collection of Bucketlists
+        .. :quickref: Bucketlists Collection; List all the created bucket lists
 
         **Example request**:
 
         .. sourcecode:: http
 
-          GET /posts/ HTTP/1.1
-          Host: example.com
+          GET /bucketlists/ HTTP/1.1
+          Host: localhost:5000
           Accept: application/json
+          Authentication: <token>
 
         **Example response**:
 
@@ -42,74 +43,112 @@ class BucketListResource(Resource):
           Vary: Accept
           Content-Type: application/json
 
-        [
+          [
             {
-                "id": 8,
-                "name": "numero 2",
+                "id": 1,
+                "name": "Before 30",
                 "items": [
                     {
-                        "id": 3,
-                        "name": "build a snow man",
-                        "date_created": "2017-07-28T16:14:27.477147",
-                        "date_modified": "2017-07-28T16:14:27.477076",
+                        "id": 1,
+                        "name": "Sky Dive",
+                        "date_created": "2017-07-30T22:29:10.044464",
+                        "date_modified": "2017-07-31T10:13:44.926045",
                         "done": false
                     }
                 ],
-                "date_created": "2017-07-28T16:14:57.853548",
-                "date_modified": "2017-07-28T16:14:57.853548",
-                "created_by": 2
+                "date_created": "2017-07-30T21:36:32.754289",
+                "date_modified": "2017-07-30T21:36:32.754289",
+                "created_by": 1
             },
             {
-                "id": 7,
-                "name": "Awesome list",
+                "id": 2,
+                "name": "Before 50",
                 "items": [
                     {
-                        "id": 4,
-                        "name": "monaco reloaded",
-                        "date_created": "2017-07-28T16:14:27.477147",
-                        "date_modified": "2017-07-29T15:57:05.707369",
-                        "done": true
+                        "id": 2,
+                        "name": "Climb Mt.Everest",
+                        "date_created": "2017-08-07T06:39:22.466605",
+                        "date_modified": "2017-08-07T06:39:22.466537",
+                        "done": false
                     }
-                ],
-                "date_created": "2017-07-28T15:26:14.349677",
-                "date_modified": "2017-07-29T16:25:58.765675",
-                "created_by": 2
-            }
-        ]
 
+                ],
+                "date_created": "2017-07-30T22:28:54.824647",
+                "date_modified": "2017-07-30T22:28:54.824647",
+                "created_by": 1
+            },
+          ]
+
+        :query q: full text search query
+        :query limit: number of bucket lists per page
+        :query page: select page
         :resheader Content-Type: application/json
-        :status 200: posts found
+        :status 200: bucketlists found
+
 
         '''
         token = request.headers.get('Authorization')
         if token:
             current_user = User.verify_auth_token(token)
         else:
-            abort(400, message='Unauthorized Access!')
+            abort(401, message='Unauthorized Access!')
 
         if not isinstance(current_user, User):
             abort(401, current_user)
 
         # get arguments
+        parser = reqparse.RequestParser()
+        parser.add_argument('q', type=str, required=False, location='args')
+        parser.add_argument('limit', type=int, required=False,
+                            default=20, help='Results per page',
+                            location='args')
+        parser.add_argument('page', type=int, default=1,
+                            help='Page number', required=False,
+                            location='args')
 
         arguments = parser.parse_args(request)
         q = arguments.get("q")
         limit = arguments.get("limit")
         page = arguments.get("page")
 
-        bucketlists = db.session.query(
-            Bucketlist).filter_by(created_by=current_user.id).all()
+        if q:
+            bucketlists = db.session.query(
+                Bucketlist.name.ilike('%' + q + '%')).filter_by(
+                created_by=current_user.id).paginate(
+                page, limit, False)
+        else:
+            bucketlists = db.session.query(
+                Bucketlist).filter_by(created_by=current_user.id).all()
 
         if bucketlists:
             # return marshal(bucketlists)
             return marshal(bucketlists, bucketlist_fields)
-            # hateos
         abort(
-            400, message='Bucketlist not found or does not belong to you.'
+            400, message='Bucketlists not found'
         )
 
     def post(self):
-        '''Adds a new bucketlist'''
+        """.. :quickref: Bucketlists Collection; Create a new bucket list.
+
+        .. sourcecode:: http
+
+          POST /bucketlists/1/ HTTP/1.1
+          Host: localhost:5000
+          Accept: application/json
+          Authentication: <token>
+
+
+        :reqheader Accept: application/json
+        :reqheader Authentication: <token>
+
+        :<json string name: bucketlist name
+
+
+        :resheader Content-Type: application/json
+        :status 201: bucketlist created
+        :status 422: invalid parameters
+        """
+
         token = request.headers.get('Authorization')
         if token:
             current_user = User.verify_auth_token(token)
@@ -144,7 +183,49 @@ class BucketListItemsResource(Resource):
 
     @marshal_with(bucketlist_fields)
     def get(self, bucketlist_id):
-        '''Displays a bucketlist specified by the id'''
+        '''.. :quickref: Bucketlist; Get single bucket list
+
+        **Example request**:
+
+        .. sourcecode:: http
+
+          GET /bucketlists/1/ HTTP/1.1
+          Host: localhost:5000
+          Accept: application/json
+
+
+        **Example response**:
+
+        .. sourcecode:: http
+
+          HTTP/1.1 200 OK
+          Vary: Accept
+          Content-Type: application/json
+          Authentication: Token
+
+          [
+            {
+                "id": 1,
+                "name": "Before 50",
+                "items": [
+                    {
+                        "id": 1,
+                        "name": "Watch F1",
+                        "date_created": "2017-07-30T22:29:10.044464",
+                        "date_modified": "2017-07-31T10:13:44.926045",
+                        "done": true
+                    }
+
+                ],
+                "date_created": "2017-07-30T21:36:32.754289",
+                "date_modified": "2017-07-30T21:36:32.754289",
+                "created_by": 1
+            },
+          ]
+
+        :resheader Content-Type: application/json
+        :status 200: bucketlist found
+        '''
         token = request.headers.get('Authorization')
         if token:
             current_user = User.verify_auth_token(token)
@@ -161,7 +242,21 @@ class BucketListItemsResource(Resource):
             abort(400, message='Expired or invalid token')
 
     def put(self, bucketlist_id):
-        '''Update the name of the bucketlist'''
+        '''.. :quickref: Bucketlist; Update this bucket list
+        .. sourcecode:: http
+
+           PUT /bucketlists/1/ HTTP/1.1
+           Host: localhost:5000
+           Accept: application/json
+           Authentication: <token>
+
+        :<json string name: Edited bucketlist name
+
+        :resheader Content-Type: application/json
+        :status 200: bucketlist updated
+        :status 422: invalid parameters
+
+        '''
         token = request.headers.get('Authorization')
         if token:
             current_user = User.verify_auth_token(token)
@@ -183,7 +278,18 @@ class BucketListItemsResource(Resource):
             return {'message': 'Expired or invalid token'}
 
     def delete(self, bucketlist_id):
-        ''''''
+        '''.. :quickref: Bucketlist; Delete this single bucket list
+
+        .. sourcecode:: http
+
+          DELETE /bucketlists/1/ HTTP/1.1
+          Host: localhost:5000
+          Accept: application/json
+          Authentication: <token>
+
+        :resheader Content-Type: application/json
+        :status 204: bucketlist deleted
+        '''
         token = request.headers.get('Authorization')
         if token:
             current_user = User.verify_auth_token(token)
@@ -200,3 +306,15 @@ class BucketListItemsResource(Resource):
                 return {'message': 'Could not find bucketlist'}
         else:
             return {'message': 'Expired or invalid token'}
+
+
+def make_port():
+    """Creates api access port."""
+
+    app = Flask(__name__)
+    api = Api(app)
+
+    api.add_resource(BucketListResource, '/bucketlists/',)
+    api.add_resource(BucketListItemsResource,
+                     '/bucketlists/<int:bucketlist_id>/')
+    return app
