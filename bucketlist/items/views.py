0e4b6edcd2e8c03bd849374ff55fba6690949f2b
charlieoduk <odukjr@gmail.com>
2017-08-07 12:00:46 +0300

    FIX -Add docstrings for documentation

--- bucketlist/items/views.py
+++ bucketlist/items/views.py
@@ -1,5 +1,5 @@
-from flask import request
-from flask_restplus import Resource, fields, abort
+from flask import request, Flask
+from flask_restplus import Resource, fields, abort, Api
 
 # local import
 from bucketlist import db
@@ -17,6 +17,25 @@ item_fields = {
 class ItemsResource(Resource):
 
     def post(self, bucketlist_id):
+        """.. :quickref: Bucketlists Collection; Add a new bucketlist item.
+
+        .. sourcecode:: http
+
+          POST /bucketlists/1/items/ HTTP/1.1
+          Host: localhost:5000
+          Accept: application/json
+          Authentication: <token>
+
+        :reqheader Accept: application/json
+
+        :<json string id: bucketlist id
+        :<json string name: bucketlist name
+
+
+        :resheader Content-Type: application/json
+        :status 201: bucketlist created
+        :status 422: invalid parameters
+        """
         token = request.headers.get('Authorization')
         if token:
             current_user = User.verify_auth_token(token)
@@ -29,11 +48,16 @@ class ItemsResource(Resource):
             bucketlist = db.session.query(Bucketlist).filter_by(
                 created_by=current_user.id, id=bucketlist_id)
             if bucketlist:
-                item = Item(name=name, bucketlist_id=bucketlist_id)
-                item.save()
+                try:
+                    item = Item(name=name, bucketlist_id=bucketlist_id)
+                    item.save()
 
-                response = {'message': 'Item successfully added to bucketlist'}
-                return response
+                    response = {
+                        'message': 'Item successfully added to bucketlist'}
+                    return response
+                except:
+                    abort(message='Failed to create item')
+
             else:
                 abort(message='Bucketlist not found')
         else:
@@ -43,6 +67,20 @@ class ItemsResource(Resource):
 class ItemsList(Resource):
 
     def put(self, bucketlist_id, item_id):
+        '''.. :quickref: Bucketlist; Update this bucket list item
+        .. sourcecode:: http
+
+           PUT /bucketlists/1/items/1/ HTTP/1.1
+           Host: localhost:5000
+           Accept: application/json
+           Authentication: <token>
+
+        :<json string name: New item name
+
+        :resheader Content-Type: application/json
+        :status 200: Item updated
+        :status 422: invalid parameters
+        '''
         token = request.headers.get('Authorization')
         if token:
             current_user = User.verify_auth_token(token)
@@ -62,7 +100,7 @@ class ItemsList(Resource):
                     item.name = name if name is not None else item.name
                     item.done = done if done is not None else item.done
                     item.save()
-                    return {'message':'Successfully updated the item'}
+                    return {'message': 'Successfully updated the item'}
                 else:
                     abort(message='Item not found')
             else:
@@ -71,6 +109,18 @@ class ItemsList(Resource):
             abort(message='Expired or invalid token')
 
     def delete(self, bucketlist_id, item_id):
+        '''.. :quickref: Bucketlist; Delete this single bucket list
+
+        .. sourcecode:: http
+
+          DELETE /bucketlists/1/items/1/ HTTP/1.1
+          Host: localhost:5000
+          Accept: application/json
+          Authentication: <token>
+
+        :resheader Content-Type: application/json
+        :status 204: bucketlist deleted
+        '''
         token = request.headers.get('Authorization')
         if token:
             current_user = User.verify_auth_token(token)
@@ -91,3 +141,15 @@ class ItemsList(Resource):
                     abort(message='Item not found')
         else:
             abort(message='Expired or invalid token')
+
+
+def make_port():
+    """Creates api access port."""
+
+    app = Flask(__name__)
+    api = Api(app)
+
+    api.add_resource(ItemsResource, '/bucketlists/<int:bucketlist_id>/items/')
+    api.add_resource(
+        ItemsList, '/bucketlists/<int:bucketlist_id>/items/<int:item_id>/')
+    return app
