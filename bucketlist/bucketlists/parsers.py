776b50ded24769c1739f84046f0b8be0549ae090
charlieoduk <odukjr@gmail.com>
2017-08-07 12:04:05 +0300

    FIX- Delete parsers.py

--- bucketlist/bucketlists/parsers.py
+++
@@ -1,6 +0,0 @@
-from flask_restplus import reqparse
-
-parser = reqparse.RequestParser()
-parser.add_argument('q', type=str, required=False, location='args')
-parser.add_argument('limit', type=int, required=False, default=20, help='Results per page', location='args')
-parser.add_argument('page', type=int, default=1, help='Page number', required=False, location='args')
